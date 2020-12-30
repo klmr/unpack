3f5e136182ab11b878a9a36068d77d807bb1c6d5
Konrad Rudolph <konrad.rudolph@gmail.com>
2020-12-30 13:58:38 +0000

    Simplify code
    The previous logic was overly complicated because I misunderstood what R
    was doing on the calling site. The new logic does away with the
    unnecessary `on.exit` handler, which was actually executed for the
    calling function anyway.

--- R/unpack.r
+++ R/unpack.r
@@ -3,59 +3,60 @@
 c = structure(list(), class = 'unpack')
 
 `[<-.unpack` = function (x, ..., value) {
-    assign('*unpack_names*', match.call(expand.dots = FALSE)$..., envir = parent.frame())
-    assign('*unpack_value*', value, envir = parent.frame())
+    names = match.call(expand.dots = FALSE)$...
+    caller = parent.frame()
 
-    eval.parent(quote(on.exit({
-        rm(c)
-        makeActiveBinding(
-            'c',
-            function (rhs) {
-                assign_all = function (names, values, envir) {
-                    valid = vapply(names, is.name, logical(1L))
-                    if (! all(valid)) {
-                        first_invalid = names[! valid][[1L]]
-                        stop(sQuote(deparse(first_invalid)), ' is not a valid variable name.')
-                    }
+    # `c` was already pulled into the caller scope at this point.
+    rm(c, envir = caller)
 
-                    names = vapply(names, deparse, character(1L))
-                    list2env(setNames(as.list(values), names), envir = envir)
+    makeActiveBinding(
+        'c',
+        function (rhs) {
+            rm(c, envir = caller)
+
+            if (length(names) > length(value)) {
+                stop('Too many names to receive ', length(value), ' values')
+            }
+
+            if (length(names) < length(value)) {
+                last = names[[length(names)]]
+
+                # Is last name of the form `name[]`?
+                if (
+                    length(last) != 3L ||
+                    ! identical(last[[1L]], as.name('[')) ||
+                    ! is.name(last[[2L]]) ||
+                    ! identical(last[[3L]], quote(expr = ))
+                ) {
+                    stop('Not enough names to receive ', length(value), ' values')
                 }
 
-                caller = sys.frame(-1L)
-                names = caller$`*unpack_names*`
-                value = caller$`*unpack_value*`
-                rm(c, `*unpack_names*`, `*unpack_value*`, envir = caller)
-
-                if (length(names) > length(value)) {
-                    stop('Too many names to receive ', length(value), ' values')
-                }
-
-                if (length(names) < length(value)) {
-                    last = names[[length(names)]]
-
-                    if (
-                        length(last) != 3L ||
-                        ! identical(last[[1L]], as.name('[')) ||
-                        ! is.name(last[[2L]]) ||
-                        ! identical(last[[3L]], quote(expr = ))
-                    ) {
-                        stop('Not enough names to receive ', length(value), ' values')
-                    }
-
-                    names = names[-length(names)]
-                    named_indices = seq_along(names)
-                    assign_all(names, value[named_indices], caller)
-                    assign(deparse(last[[2L]]), value[-named_indices], caller)
-                } else {
-                    assign_all(names, value, caller)
-                }
-            },
-            environment()
-        )
-    })))
+                names = names[-length(names)]
+                named_indices = seq_along(names)
+                assign_all(names, value[named_indices], caller)
+                assign(deparse(last[[2L]]), value[-named_indices], caller)
+            } else {
+                assign_all(names, value, caller)
+            }
+        },
+        caller
+    )
 
     invisible(x)
+}
+
+# Helper function that assigns a number of values to names, after checking that
+# the list of expressions provided as names are actually valid R name
+# expressions (via `is.name`).
+assign_all = function (names, values, envir) {
+    valid = vapply(names, is.name, logical(1L))
+    if (! all(valid)) {
+        first_invalid = names[! valid][[1L]]
+        stop(sQuote(deparse(first_invalid)), ' is not a valid variable name.')
+    }
+
+    names = vapply(names, deparse, character(1L))
+    list2env(stats::setNames(as.list(values), names), envir = envir)
 }
 
 print.unpack = function (x, ...) {
