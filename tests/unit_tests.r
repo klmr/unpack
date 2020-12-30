db10ded8e443ad2d774db76aa229844c1d7f97f8
Konrad Rudolph <konrad.rudolph@gmail.com>
2020-12-30 00:05:32 +0000

    Add unit test for usage inside function

--- tests/unit_tests.r
+++ tests/unit_tests.r
@@ -14,5 +14,13 @@ assert(all.equal(sort(ls()), c('feb', 'jan', 'rest')))
 
 assert(jan == 'January')
 assert(feb == 'February')
-message(rest)
 assert(all.equal(rest,  month.name[- (1 : 2)]))
+
+f = function () {
+    c[a, b, c] = 1 : 3
+    assert(a == 1L)
+    assert(b == 2L)
+    assert(c == 3L)
+}
+
+f()
